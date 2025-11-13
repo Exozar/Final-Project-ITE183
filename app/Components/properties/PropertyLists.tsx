72f489918e150eeee5eeb5c8d24011c7a534437a
Bryan Salazar <bryanjethro.salazar@msunaawan.edu.ph>
2025-11-13 13:31:00 +0800

    Propert List_Salazar

---
+++ app/Components/properties/PropertyLists.tsx
@@ -0,0 +1,12 @@
+import PropertyListItem from "./PropertyListItems";
+
+const PropertyList = () => {
+    return (
+        <>
+            <PropertyListItem />
+            <PropertyListItem />
+            <PropertyListItem />
+        </>
+    )
+}
+export default PropertyList;
