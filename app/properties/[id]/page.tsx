e29284b1163a2f26a440e5286c48be5ef59897ea
Bryan Salazar <bryanjethro.salazar@msunaawan.edu.ph>
2025-11-13 13:59:05 +0800

    page transfer to /properties/id folder

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import ReservationSidebar from "../Components/properties/ReservationSideBar";
+import ReservationSidebar from "../../Components/properties/ReservationSideBar";
 
 const PropertyDetailPage = () => {
     return (
