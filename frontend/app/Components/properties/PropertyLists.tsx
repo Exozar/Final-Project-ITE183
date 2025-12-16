5bcf856d85453108acdbfc70bfb1f0fcc53b2f00
Bryan Salazar <bryanjethro.salazar@msunaawan.edu.ph>
2025-12-17 00:20:12 +0800

    My Favorite page (with listings)__Salazar

--- frontend/app/Components/properties/PropertyLists.tsx
+++ frontend/app/Components/properties/PropertyLists.tsx
@@ -57,6 +57,8 @@ const PropertyLists: React.FC<PropertyListProps> = ({
 
     if (landlord_id) {
         url += `?landlord_id=${landlord_id}`
+    } else if (favorites) {
+        url += '?is_favorites=true'
     }
 
 
